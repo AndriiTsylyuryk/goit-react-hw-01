1a3a2f1125c893a5d8a9ae901196475f00e2ce3a
AndriiTsylyuryk <navigator.tsylyuryk@gmail.com>
2024-06-28 15:19:37 +0300

    json created

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,9 @@
-import Alert from "./Alert/Alert";
-import LoginButton from "./Button/Button";
-
+import { Profile } from "./Profile/Profile";
 
 
 const App = () => {
   return (
-    <main>
-      <Alert variant="info">
-        Would you like to browse our recommended products?
-      </Alert>
-      <Alert variant="error" outlined={true}>
-        There was an error during your last transaction
-      </Alert>
-      <Alert variant="success">
-        Payment received, thank you for your purchase
-      </Alert>
-      <Alert variant="warning">
-        Please update your profile contact information
-      </Alert>
-      <LoginButton variant="primary" />
-    </main>
+   <Profile />
   );
 };
 
